38144c92be47ccfc87ccd6672c9361c7c4805f48
Ahmad Aziz <ahamadaziz7@gmail.com>
2021-09-07 17:33:06 +0500

    created local database and server that connects to the database

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import express from "express";
+const express = require("express");
+require("./db/mongoose");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(port, () => console.log(`Server is up on port ${port}`));
