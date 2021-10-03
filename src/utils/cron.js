d009eeae0f13fc55f70366cc773a19dedf1593a6
Ahmad Aziz <ahamadaziz7@gmail.com>
2021-10-03 16:52:24 +0500

    voting timeLimit is now being checked using cronjob

--- src/utils/cron.js
+++ src/utils/cron.js
@@ -1,14 +1,24 @@
 const Poll = require("../models/poll-model");
 const CronJob = require("cron").CronJob;
+const dayjs = require("dayjs");
 
-const checkVoteTimeLimit = new CronJob("* * * * *", async () => {
-  const currentDate = new Date.now();
-  const polls = await Poll.find({ voteEnded: false });
-  const expiredPolls = polls.map((poll) =>
-    currentDate - poll.createdAt > 7
-      ? (poll.voteEnded = true)
-      : (poll.voteEnded = false)
-  );
+const checkVoteTimeLimit = new CronJob("* * * * *", async function () {
+  try {
+    const polls = await Poll.find({ voteEnded: false });
+    if (!polls) throw new Error("Invalid search or no polls found!");
+    polls.forEach((poll) => {
+      const condition = poll.timeLimit.split(":");
+      const check = dayjs().diff(poll.createdAt, condition[0]) >= condition[1];
+      if (check) {
+        poll.voteEnded = true;
+      } else {
+        return;
+      }
+    });
+    await polls.save();
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = checkVoteTimeLimit;
